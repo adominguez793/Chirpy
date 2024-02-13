72dc799a81196ef72b5ab37ebadf656f1382f83f
Alejandro Dominguez <alejandrodominguez@Alejandros-MacBook-Air.local>
2024-02-13 16:44:50 -0500

    Added servers and routing

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/adominguez793/Chirpy
+
+go 1.22.0
+
+require github.com/go-chi/chi/v5 v5.0.11
+
+//indirect  <<-- that text was to the right of the line above this one
+// and it was causing an error for some reason
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	apiCfg := apiConfig{
+		fileserverHits: 0,
+	}
+
+	r := chi.NewRouter()
+
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	r.Handle("/app/*", fsHandler) // '/app/' could be --> '/app/*' && vice versa
+	r.Handle("/app", fsHandler)
+
+	r.Get("/healthz", handlerReadiness)
+	r.Get("/metrics", apiCfg.handlerMetrics)
+	r.Get("/reset", apiCfg.handlerReset)
+
+	corsMux := middlewareCors(r)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsMux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
